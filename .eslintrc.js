d5f0393ef230ed6947a2f6951005a077e33dd670
Daniel Nixon <dan.nixon@gmail.com>
2022-09-03 20:23:26 +1000

    Dogfood

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,43 +1,44 @@
 module.exports = {
-    parser: "@typescript-eslint/parser",
-    parserOptions: {
-      project: "./tsconfig.json",
-      ecmaVersion: 2018,
-      sourceType: "module"
-    },
-    extends: [
-      "agile-digital"
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "./tsconfig.json",
+    ecmaVersion: 2018,
+    sourceType: "module",
+  },
+  extends: ["agile-digital", "agile-digital/react"],
+  env: {
+    "jest/globals": true,
+    es6: true,
+  },
+  plugins: [
+    "jest",
+    "sonarjs",
+    "functional",
+    "@typescript-eslint",
+    "prettier",
+    "total-functions",
+    "import",
+    "spellcheck",
+    "react",
+    "react-hooks",
+    "jsx-a11y",
+  ],
+  rules: {
+    "spellcheck/spell-checker": [
+      1,
+      {
+        skipWords: [
+          "globals",
+          "commonjs",
+          "sonarjs",
+          "tsconfig",
+          "ecma",
+          "fp",
+          "a11y",
+          "jsx",
+        ],
+      },
     ],
-    env: {
-      "jest/globals": true,
-      es6: true
-    },
-    plugins: [
-      "jest",
-      "sonarjs",
-      "functional",
-      "@typescript-eslint",
-      "prettier",
-      "total-functions",
-      "import",
-      "spellcheck",
-    ],
-    rules: {
-      "spellcheck/spell-checker": [1,
-        {
-          skipWords: [
-            "globals",
-            "commonjs",
-            "sonarjs",
-            "tsconfig",
-            "ecma",
-            "fp",
-            "a11y",
-            "jsx",
-          ]
-        }
-      ]
-    },
-    settings: {},
-  };
-  
+  },
+  settings: {},
+};
